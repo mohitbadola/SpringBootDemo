c5167811b6b76edd83dccab365924fdcf3ddbf40
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-08-28 20:24:17 +0530

    Initial Commit

---
+++ src/main/java/com/jason/app/Alien.java
@@ -0,0 +1,15 @@
+package com.jason.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+    @Autowired
+    Laptop laptop;
+
+    public void code(){
+        laptop.compile();
+        System.out.println("Coding");
+    }
+}
